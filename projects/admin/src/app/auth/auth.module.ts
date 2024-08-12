7521fb669b3fafb66f6e1e878ccf46940f203b8a
abanoub maged <abanoubmagedmaher@gmail.com>
2024-08-12 23:49:33 +0300

    Handel Login

--- projects/admin/src/app/auth/auth.module.ts
+++ projects/admin/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     LoginComponent
   ],
   imports: [
+    MaterialModule,
     CommonModule,
     AuthRoutingModule,
     //First Party Modules
