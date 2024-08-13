837868cb09e8aac4e48dbb6595251cadda630592
abanoub maged <abanoubmagedmaher@gmail.com>
2024-08-13 20:20:19 +0300

    Handel

--- projects/admin/src/app/app-routing.module.ts
+++ projects/admin/src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>import(`./dashboard/dashboard.module`).then(m=>m.DashboardModule)
+  },
   {path:'login',
     loadChildren:()=>import(`./auth/auth.module`).then(m=>m.AuthModule)
   }
---
+++ projects/admin/src/app/dashboard/tasks-admin/tasks-admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TasksAdminRoutingModule } from './tasks-admin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TasksAdminRoutingModule
+  ]
+})
+export class TasksAdminModule { }
