8faa9942898503f9b82cc3be0461ab19d8286b13
abanoub maged <abanoubmagedmaher@gmail.com>
2024-08-14 21:22:32 +0300

    Handel

--- projects/admin/src/app/dashboard/tasks-admin/tasks-admin.module.ts
+++ projects/admin/src/app/dashboard/tasks-admin/tasks-admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TasksAdminRoutingModule } from './tasks-admin-routing.module';
+import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
+import { ListTasksComponent } from './components/list-tasks/list-tasks.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddTasksComponent,
+    ListTasksComponent
+  ],
   imports: [
     CommonModule,
     TasksAdminRoutingModule
